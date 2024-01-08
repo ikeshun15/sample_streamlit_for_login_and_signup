442053f469d09b77d56539f18dbe8bf3241f2cc4
shun15 <ikezushunichi0715@gmail.com>
2024-01-08 19:01:40 +0900

    partial change

--- controller/manager/login_manager.py
+++ controller/manager/login_manager.py
@@ -1,25 +1,10 @@
-import hashlib
-
+from ..handler import HashHandler
 from model import UserInfoManager, UserInfoEntity
 
 class LoginManager:
     @staticmethod
-    def hash_password(password: str) -> str:
-        # パスワードをバイト列に変換する
-        password_bytes = password.encode('utf-8')
-
-        # ハッシュ化アルゴリズムを選択する
-        hash_algorithm = hashlib.sha256()
-
-        # パスワードをハッシュ化する
-        hash_algorithm.update(password_bytes)
-
-        # ハッシュ化されたパスワードを返す
-        return hash_algorithm.hexdigest()
-
-    @classmethod
-    def login(cls, username: str, password: str) -> tuple[bool, str, UserInfoEntity]:
-        hashed_password = cls.hash_password(password=password)
+    def login(username: str, password: str) -> tuple[bool, str, UserInfoEntity]:
+        hashed_password = HashHandler.hash_password(password=password)
         registered_users = UserInfoManager.get_registered_users_dict()
         user_info_instance = UserInfoEntity(username=username, hashed_password=hashed_password)
 
